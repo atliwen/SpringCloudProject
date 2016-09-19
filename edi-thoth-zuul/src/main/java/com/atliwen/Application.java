5aff1a1c96bb8888994a696a46d82611471a2878
atliwen <499014099@qq.com>
2016-09-19 14:00:55 +0800

    服务网关

---
+++ edi-thoth-zuul/src/main/java/com/atliwen/Application.java
@@ -0,0 +1,22 @@
+package com.atliwen;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class Application
+{
+	public static void main(String[] args)
+	{
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+
+	// @Bean
+	// public AccessFilter accessFilter()
+	// {
+	// return new AccessFilter();
+	// }
+
+}
